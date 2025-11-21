e6a0642e0d849c39a3a6287c47c74bb4a0ea48af
alugoro <putrafarrel3030@gmail.com>
2025-11-21 20:01:46 +0700

    Update CORS

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://lestari-store-frontend.vercel.app/',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
